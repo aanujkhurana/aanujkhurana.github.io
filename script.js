70045d4ad986172f1c4f9a783952671ae3905cb9
aanujkhurana <aanujkhurana@gmail.com>
2024-02-05 14:21:08 +1000

    layout+design ✅

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const triggerPosition = 0;
 
     if (scrollPosition >= triggerPosition) {
-        home.style.opacity = 1 - (scrollPosition - triggerPosition) / 100;
+        home.style.opacity = 1 - (scrollPosition - triggerPosition) / 300;
     } else {
         home.style.opacity = 1;
     }
